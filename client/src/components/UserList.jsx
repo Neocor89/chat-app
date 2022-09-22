e56bd821c458b9d921b2d4de3839f826292ed63c
Ben <ncor89b@gmail.com>
2022-09-22 21:03:40 +0200

    user creation and threading

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect }from 'react';
-import { Avatar, useChatChannel } from 'stream-chat-react'
+import { Avatar, useChatContext } from 'stream-chat-react'
 
 import { InviteIcon } from '../assets'
 
+//: Listing and descript User
 const ListContainer = ({children}) => {
   return (
     <div className="user-list__container">
@@ -17,21 +18,66 @@ const ListContainer = ({children}) => {
 
 //: Users List Components
 
-const UserItem = () => {
+const UserItem = ({ user }) => {
+  const [selected, setSelected] = useState(false);
+
+  const handleSelect = () => {
+    setSelected((prevSelected) => !prevSelected);
+  }
   return (
-    <div className="user-item__wrapper">
-      {/* <div className="user-list__header">
-        <p>Usertem</p>
-      </div> */}
+    <div className="user-item__wrapper" onClick={handleSelect}>
+      <div className="user-list__wrapper">
+        <Avatar image={user.image} name={user.fullName || user.id} size={32}  />
+        <p className="user-item__name">{user.fullName || user.id}</p>
+      </div>
+      {selected ? <InviteIcon /> : <div className="user-item__invite-empty"/>}
     </div>
   )
 }
 
 const UserList = () => {
+  const { client } = useChatContext();
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [listEmpty, setListEmpty] = useState(false);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      if(loading) return;
+
+      setLoading(true);
+
+      try {
+        const response = await client.queryUsers(
+          { id: { $ne: client.userID } },
+          { id: 1 },
+          { limit: 8 }
+        );
+
+        if(response.users.length) {
+          setUsers(response.users);
+        } else {
+          setListEmpty(true);
+        }
+        
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+    if(client) getUsers()
+  }, [])
+  
   return (
-    <div className="">
-      UserList
-      </div>
+    <ListContainer>
+      {loading ? <div className="user-list__message">
+        Loading users...
+      </div> : (
+        users?.map((user, i) => (
+          <UserItem index={i} key={user.id} user={user}/>
+        ))
+      )}
+      </ListContainer>
   )
 }
 
